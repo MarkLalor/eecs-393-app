e245e002406eeca937116c3d7ed951e1e9342bc3
Mark Lalor <markwlalor@gmail.com>
2017-03-08 15:58:24 -0500

    Added web elements and config skeleton.

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import webapp2
+import json
 
 class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('eecs-393-app! using webapp2')
+	@staticmethod
+	def html_print(request, filename, *parts):
+		with open('web/html/' + filename + '.html', 'r') as content_file:
+			request.response.out.write(content_file.read() % parts)
+			request.response.out.write("\n")
 
+	def get(self):
+		self.response.headers['Content-Type'] = 'text/html'
+
+		config = {}
+		with open('config.json', 'r') as config_file:
+			config = json.load(config_file)
+
+		MainPage.html_print(self, 'page_header')
+		MainPage.html_print(self, 'body_header', config['header'])
+		MainPage.html_print(self, 'body')
+		MainPage.html_print(self, 'body_footer')
+		MainPage.html_print(self, 'page_footer')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
