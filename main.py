dc3c644e1911f3d0e0dcb3f6c28aa81286cda96b
vsocrates <vimig.socrates@gmail.com>
2017-03-29 21:36:28 -0400

    added jinja2 template support/added support for canvas api

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import webapp2
 import json
+import os
+
 from google.appengine.api import users
+import urllib2
+from canvas_sdk import RequestContext
+from canvas_sdk.methods import sections
+from canvas_sdk.methods import courses
+
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+''' python dev_appserver.py C://Vimig/SoftwareEng/mainApp/eecs-393-app/app.yaml '''
 
 class MainPage(webapp2.RequestHandler):
-	@staticmethod
-	def html_print(request, filename, *parts):
-		with open('web/html/' + filename + '.html', 'r') as content_file:
-			request.response.out.write(content_file.read() % parts)
-			request.response.out.write("\n")
+	# @staticmethod
+	# def html_print(request, filename, *parts):
+	# 	with open('web/html/' + filename + '.html', 'r') as content_file:
+	# 		print content_file.read() % parts
+	# 		request.response.out.write(content_file.read() % parts)
+	# 		request.response.out.write("\n")
 
 	def get(self):
 
@@ -17,6 +32,14 @@ class MainPage(webapp2.RequestHandler):
 		if not user:
 			login_url = users.create_login_url('/')
 			self.redirect(login_url)
+
+		oauth_token = "5590~AiNzEe2sqfpgQuKEn6vCoHUG1x8PTt0VniscrvG8gSp0hke4OW5jciN0maBxm9QQ"
+		base_api_url = 'https://canvas.case.edu/api'
+		
+		rc = RequestContext(oauth_token, base_api_url)
+		results = courses.list_your_courses(rc, "needs_grading_count")
+		courseJSON = results.content
+
 
 		nickname = user.nickname() #for debugging
 		logout_url = users.create_logout_url('/');
@@ -28,11 +51,14 @@ class MainPage(webapp2.RequestHandler):
 		with open('config.json', 'r') as config_file:
 			config = json.load(config_file)
 
-		MainPage.html_print(self, 'page_header')
-		MainPage.html_print(self, 'body_header', config['header'],nickname,logout_url)
-		MainPage.html_print(self, 'body')
-		MainPage.html_print(self, 'body_footer')
-		MainPage.html_print(self, 'page_footer')
+		template_values = {
+			'config_header': config['header'],
+			'nickname': nickname,
+			'logout_url': logout_url,
+		}
+
+		template = JINJA_ENVIRONMENT.get_template('web/html/body.html')
+		self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
